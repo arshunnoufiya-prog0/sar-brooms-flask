aae112b5d5be5d519196fc0cd7bc139f18a773c9
Arshun Noufiya <arshunnoufiya@gmail.com>
2026-06-28 21:45:50 +0530

    fix deployment

--- app.py
+++ app.py
@@ -530,4 +530,4 @@ def init_db_and_seed():
 
 if __name__ == "__main__":
     init_db_and_seed()
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
